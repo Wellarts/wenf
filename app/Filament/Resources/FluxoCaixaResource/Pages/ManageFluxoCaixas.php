675a04a14d839c300231600e480ac47418cf2881
Wellington Cavalcante <wellarts@gmail.com>
2023-09-01 18:11:59 -0300

    upt

--- app/Filament/Resources/FluxoCaixaResource/Pages/ManageFluxoCaixas.php
+++ app/Filament/Resources/FluxoCaixaResource/Pages/ManageFluxoCaixas.php
@@ -14,7 +14,8 @@ class ManageFluxoCaixas extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Novo'),
         ];
     }
 
