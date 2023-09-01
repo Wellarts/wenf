b371c5949c3a873ca50ea370455c24e01d164982
Wellington Cavalcante <wellarts@gmail.com>
2023-09-01 17:34:30 -0300

    agendamentos

--- app/Livewire/ListaAgendamentos.php
+++ app/Livewire/ListaAgendamentos.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\AgendarAtendimento;
+use Carbon\Carbon;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -21,17 +22,34 @@ class ListaAgendamentos extends BaseWidget
                 AgendarAtendimento::query()->where('status', 0)->orderby('data_atendimento', 'asc'),
             )
             ->columns([
-                Tables\Columns\TextColumn::make('nome')
-                    ->searchable(),
+                Tables\Columns\TextColumn::make('nome'),
                 Tables\Columns\TextColumn::make('data_atendimento')
                     ->label('Data do Atendimento')
                     ->alignCenter()
                     ->date()
-                    ->sortable(),
+                    ->badge()
+                    ->color(static function ($state): string {
+                        $hoje = Carbon::today();
+                        $dataAgendamento = Carbon::parse($state);
+                        $qtd_dias = $hoje->diffInDays($dataAgendamento, false);
+
+                        if ($qtd_dias <= 3 && $qtd_dias >= 0) {
+                            return 'danger';
+                        }
+
+                        if($qtd_dias < 0) {
+                            return 'warning';
+                        }
+
+                        if($qtd_dias > 3) {
+                            return 'success';
+                        }
+
+                    }),
                 Tables\Columns\TextColumn::make('hora_atendimento')
                     ->label('Hora do Atendimento')
-                    ->alignCenter()
-                    ->searchable(),
+                    ->alignCenter(),
+
             ]);
     }
 }
